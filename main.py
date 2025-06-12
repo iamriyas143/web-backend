3aed4ab93c90761b9cea650d9a460f2bf9510451
iamriyas143 <iamriyas1727@gmail.com>
2025-06-12 20:35:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+import os
 
 app = Flask(__name__)
 CORS(app)  # Allow Netlify frontend
@@ -49,3 +50,7 @@ def receive_location():
 @app.route("/")
 def home():
     return "Location Logger API with Telegram is running.", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
